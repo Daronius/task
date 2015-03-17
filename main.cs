714ec4c91178411b2b3f419ec64caaf1343e030f
Daronius <Daronius@users.noreply.github.com>
2015-03-17 20:10:24 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,7 @@ namespace Task {
     class main {
         public static void Main() {
             Console.WriteLine("Hello Task!");
+            Console.WriteLine("We will create you.");
         }
     }
 }
