e796b0b1b835feb950eb8ea4b733b41c3cdb4cf2
Daronius <Daronius@users.noreply.github.com>
2015-03-17 17:03:16 +0100

    C# Project created

---
+++ main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task {
+    class main {
+        public static void Main() {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
