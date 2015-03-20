8f6d4abc622e5559d0fc0625e56b327414650c0f
Daronius <Daronius@users.noreply.github.com>
2015-03-20 01:40:14 +0100

    WebServer improved WebServer creates folder structure WebServer creates index.htm

--- WebServer.cs
+++ WebServer.cs
@@ -1,73 +1,212 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Sockets;
 using System.Net;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Task {
     class WebServer {
         Log log = Log.getInstance();
-        HttpListener listener;
 
-        public WebServer(string[] prefixes) {
-            try {
-                if(!HttpListener.IsSupported) {
-                    log.debugWrite("[WebServer] Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
-                    return;
+        private readonly string[] _indexFiles = {
+            "index.html",
+            "index.htm",
+            "default.html",
+            "default.htm"
+        };
+
+        private static IDictionary<string, string> _mimeTypeMappings = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
+            #region extension to MIME type list
+            {".asf", "video/x-ms-asf"},
+            {".asx", "video/x-ms-asf"},
+            {".avi", "video/x-msvideo"},
+            {".bin", "application/octet-stream"},
+            {".cco", "application/x-cocoa"},
+            {".crt", "application/x-x509-ca-cert"},
+            {".css", "text/css"},
+            {".deb", "application/octet-stream"},
+            {".der", "application/x-x509-ca-cert"},
+            {".dll", "application/octet-stream"},
+            {".dmg", "application/octet-stream"},
+            {".ear", "application/java-archive"},
+            {".eot", "application/octet-stream"},
+            {".exe", "application/octet-stream"},
+            {".flv", "video/x-flv"},
+            {".gif", "image/gif"},
+            {".hqx", "application/mac-binhex40"},
+            {".htc", "text/x-component"},
+            {".htm", "text/html"},
+            {".html", "text/html"},
+            {".ico", "image/x-icon"},
+            {".img", "application/octet-stream"},
+            {".iso", "application/octet-stream"},
+            {".jar", "application/java-archive"},
+            {".jardiff", "application/x-java-archive-diff"},
+            {".jng", "image/x-jng"},
+            {".jnlp", "application/x-java-jnlp-file"},
+            {".jpeg", "image/jpeg"},
+            {".jpg", "image/jpeg"},
+            {".js", "application/x-javascript"},
+            {".mml", "text/mathml"},
+            {".mng", "video/x-mng"},
+            {".mov", "video/quicktime"},
+            {".mp3", "audio/mpeg"},
+            {".mpeg", "video/mpeg"},
+            {".mpg", "video/mpeg"},
+            {".msi", "application/octet-stream"},
+            {".msm", "application/octet-stream"},
+            {".msp", "application/octet-stream"},
+            {".pdb", "application/x-pilot"},
+            {".pdf", "application/pdf"},
+            {".pem", "application/x-x509-ca-cert"},
+            {".pl", "application/x-perl"},
+            {".pm", "application/x-perl"},
+            {".png", "image/png"},
+            {".prc", "application/x-pilot"},
+            {".ra", "audio/x-realaudio"},
+            {".rar", "application/x-rar-compressed"},
+            {".rpm", "application/x-redhat-package-manager"},
+            {".rss", "text/xml"},
+            {".run", "application/x-makeself"},
+            {".sea", "application/x-sea"},
+            {".shtml", "text/html"},
+            {".sit", "application/x-stuffit"},
+            {".swf", "application/x-shockwave-flash"},
+            {".tcl", "application/x-tcl"},
+            {".tk", "application/x-tcl"},
+            {".txt", "text/plain"},
+            {".war", "application/java-archive"},
+            {".wbmp", "image/vnd.wap.wbmp"},
+            {".wmv", "video/x-ms-wmv"},
+            {".xml", "text/xml"},
+            {".xpi", "application/x-xpinstall"},
+            {".zip", "application/zip"},
+            #endregion
+        };
+
+        private Thread _serverThread;
+        private string _rootDirectory;
+        private HttpListener _listener;
+        private int _port;
+        public int Port {
+            get {
+                return _port;
+            }
+            private set {
+            }
+        }
+
+        /// <summary>
+        /// Construct server with given port.
+        /// </summary>
+        /// <param name="path">Directory path to serve.</param>
+        /// <param name="port">Port of the server.</param>
+        public WebServer(string path, int port) {
+            this.Initialize(path, port);
+        }
+
+        /// <summary>
+        /// Construct server with suitable port.
+        /// </summary>
+        /// <param name="path">Directory path to serve.</param>
+        public WebServer(string path) {
+            //get an empty port
+            TcpListener l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            int port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
+            this.Initialize(path, port);
+        }
+
+        /// <summary>
+        /// Stop server and dispose all functions.
+        /// </summary>
+        public void Stop() {
+            _serverThread.Abort();
+            _listener.Stop();
+        }
+
+        private void Listen() {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://*:" + _port.ToString() + "/");
+            _listener.Start();
+            while(true) {
+                try {
+                    HttpListenerContext context = _listener.GetContext();
+                    Process(context);
                 }
-                if(prefixes == null || prefixes.Length == 0)
-                    throw new ArgumentException("missing prefixes");
-
-                listener = new HttpListener();
-
-                foreach(string s in prefixes) {
-                    listener.Prefixes.Add(s);
-                    log.debugWrite("[WebServer] listen on " + s);
+                catch(Exception ex) {
                 }
-
-                listener.Start();
-                log.debugWrite("[WebServer] Listening");
-
-                listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
-            }
-            catch(Exception e) {
-                log.debugWrite("[WebServer] " + e.ToString());
             }
         }
 
-        private void ListenerCallback(IAsyncResult Result) {
-            try {
-                HttpListenerContext context = listener.EndGetContext(Result);
-                HttpListenerRequest request = context.Request;
-                HttpListenerResponse response = context.Response;
-
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(createResponse());
-                response.ContentLength64 = buffer.Length;
-
-                Stream output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                output.Close();
+        private void Process(HttpListenerContext context) {
+            string filename = context.Request.Url.AbsolutePath;
+            Console.WriteLine(filename);
+            filename = filename.Substring(1);
+            if(string.IsNullOrEmpty(filename)) {
+                foreach(string indexFile in _indexFiles) {
+                    if(File.Exists(Path.Combine(_rootDirectory, indexFile))) {
+                        filename = indexFile;
+                        break;
+                    }
+                }
             }
-            catch(InvalidOperationException eR) {
-                Console.WriteLine("[WebServer]" + eR.ToString());
+            filename = Path.Combine(_rootDirectory, filename);
+            if(File.Exists(filename)) {
+                try {
+                    Stream input = new FileStream(filename, FileMode.Open);
+                    //Adding permanent http response headers
+                    string mime;
+                    context.Response.ContentType = _mimeTypeMappings.TryGetValue(Path.GetExtension(filename), out mime) ? mime : "application/octet-stream";
+                    context.Response.ContentLength64 = input.Length;
+                    context.Response.AddHeader("Date", DateTime.Now.ToString("r"));
+                    context.Response.AddHeader("Last-Modified", System.IO.File.GetLastWriteTime(filename).ToString("r"));
+                    byte[] buffer = new byte[1024 * 16];
+                    int nbytes;
+                    while((nbytes = input.Read(buffer, 0, buffer.Length)) > 0)
+                        context.Response.OutputStream.Write(buffer, 0, nbytes);
+                    input.Close();
+                    context.Response.OutputStream.Flush();
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                }
+                catch(Exception ex) {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                }
+            } else {
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
             }
-            try {
-                listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
-            }
-            catch(InvalidOperationException eR) {
-                Console.WriteLine("[WebServer]" + eR.ToString());
-            }
+            context.Response.OutputStream.Close();
         }
 
-        private string createResponse() {
-            string response;
-            response = "<HTML><HEAD><TITLE>WebServer</TITLE></HEAD><BODY><b>Hello</b> Task!</BODY></HTML>";
-            return response;
-        }
-
-        ~WebServer() {
-            listener.Stop();
+        private void Initialize(string path, int port) {
+            if(!Directory.Exists(path)) {
+                Directory.CreateDirectory(path);
+                log.debugWrite("[WebServer] Folder htdocs\\ created");
+            }
+            if(!File.Exists(path + "index.htm")) {
+                //File.Create(path + "index.htm");
+                string[] lines = { "<html>",
+                                   "<head>",
+                                   "<title>Task</title>",
+                                   "</head>",
+                                   "<body>",
+                                   "<h1>Task</h1>",
+                                   "<hr>",
+                                   "<p>Task WebServer is working.</p>",
+                                   "</body>",
+                                   "</html>" };
+                System.IO.File.WriteAllLines(path + "index.htm", lines);
+                log.debugWrite("[WebServer] File index.htm created");
+            }
+            this._rootDirectory = path;
+            this._port = port;
+            _serverThread = new Thread(this.Listen);
+            _serverThread.Start();
         }
     }
 }
--- main.cs
+++ main.cs
@@ -6,8 +6,9 @@ using System.Text;
 namespace Task {
     class main {
         public static void Main() {
-            string[] prefixes = { "http://*:8080/" };
-            new WebServer(prefixes);
+            Log log = Log.getInstance();
+
+            new WebServer(AppDomain.CurrentDomain.BaseDirectory + "htdocs\\" ,8080);
             while(true) {
             }
         }
